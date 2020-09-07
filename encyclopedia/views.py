b94e56606a2ac37f7d04d4aab55519e80afa4efa
petryshend <petryshend@gmail.com>
2020-09-07 11:55:38 +0300

    View entry implemented

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,6 @@
+from django.http import Http404
 from django.shortcuts import render
+from markdown2 import markdown
 
 from . import util
 
@@ -8,3 +10,12 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def view_entry(request, entry_name):
+    entry_content = util.get_entry(entry_name)
+    if not entry_content:
+        raise Http404('Entry does not exists')
+    return render(request, 'encyclopedia/view_entry.html', {
+        'entry_name': entry_name,
+        'entry_content': markdown(entry_content)
+    })
